3cfdac2470a5d33b88bed372771b8b2533f067f5
atsung1 <atsung1@babson.edu>
2018-12-04 13:55:18 -0500

    started flask file

--- frontend.py
+++ frontend.py
@@ -11,20 +11,19 @@ app.secret_key = "Some secret string here"
 def index():
     return render_template('index.html')
 
-@app.route('/csvpage/')
-def csvpage():
-    return render_template('csvpage.html')
-
 @app.route('/matchpage/')
 def matchpage():
+    #users will upload photos on this page
     return render_template('matchpage.html')
 
 @app.route('/confirmation/')
 def confirmation():
+    #this page will display a color grid, and users will select which color
     return render_template('confirmation.html')
 
 @app.route('/results/')
 def results():
+    #we will return top 3 results with color grid and product name
     return render_template('results.html')
 
 # if __name__ == '__main__':
