3739c72fca545b52e37eae0dd5283ab49befa289
atsung1 <atsung1@babson.edu>
2018-12-04 15:57:51 -0500

    tried some upload stuff

--- frontend.py
+++ frontend.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'C:\Users\atsung1\Documents\Software Design\MIS3640Project'
+
 app.config['DEBUG'] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.secret_key = "Some secret string here"
 
@@ -11,11 +14,31 @@ app.secret_key = "Some secret string here"
 def index():
     return render_template('index.html')
 
-@app.route('/matchpage/')
+@app.route('/matchpage', methods=['GET', 'POST'])
 def matchpage():
-    #users will upload photos on this page
-    return render_template('matchpage.html')
+    if request.method == 'POST':
+        #check if post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submits a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+        #f = request.files['the_file']
+        file.save('/var/www/uploads/uploaded_file.txt')
+        return render_template('matchpage.html')
+
+#users will upload photos on this page
 #will send file back to python and return grid of colors r1 g1 b1 r2 g2 b2 ......
+
 
 @app.route('/confirmation/')
 def confirmation():
