5f19d14ea9df22e9479ebdcab77afd111e60a406
atsung1 <atsung1@babson.edu>
2018-12-04 15:23:20 -0500

    Added Notes

--- frontend.py
+++ frontend.py
@@ -15,15 +15,18 @@ def index():
 def matchpage():
     #users will upload photos on this page
     return render_template('matchpage.html')
+#will send file back to python and return grid of colors r1 g1 b1 r2 g2 b2 ......
 
 @app.route('/confirmation/')
 def confirmation():
     #this page will display a color grid, and users will select which color
     return render_template('confirmation.html')
 
+#send the selection to python, pick appropriate RGB, calculate top 3 results
 @app.route('/results/')
 def results():
     #we will return top 3 results with color grid and product name
+    #s
     return render_template('results.html')
 
 # if __name__ == '__main__':
